7fd63ab1c1398644b7eae29cfd099bd4f7246a3f
Joshua-Santeliz <joshualeonardo2007@gmail.com>
2026-03-07 00:29:22 -0400

    cargar script

---
+++ prueba de commit.R
@@ -0,0 +1,12 @@
+plantas <- c(15,5,5,6,1,4)
+plantas <- c("eucalito",6,4 )
+class (plantas)
+ls()
+print(plantas)
+length(plantas)
+"hola R"
+print
+saludo <- "hola R"
+print(hola R)
+length(saludo)
+class(plantas)
